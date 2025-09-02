601acf9910f0bc9327f7a5377045356be131543b
nishankx <nishankashyapofficial@gmail.com>
2025-09-02 13:48:14 +0530

    registering Tweet model into Admin

--- dotcom/tweet/views.py
+++ dotcom/tweet/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def show_tweets(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html',{'tweets': tweets})
