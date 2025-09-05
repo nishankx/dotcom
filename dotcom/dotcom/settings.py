0a242ed78b8bf5629aaf56622d41dc2e0ed40ab7
nishankx <nishankashyapofficial@gmail.com>
2025-09-05 18:17:20 +0530

    Fixed Login and Register Redirecting Links

--- dotcom/dotcom/settings.py
+++ dotcom/dotcom/settings.py
@@ -130,5 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login'
-LOGIN_REDIRECT_URL = '/tweet'
-LOGOUT_REDIRECT_URL = '/tweet'
+LOGIN_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = ''
